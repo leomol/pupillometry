1290976b9647a6d70e13084f3a3785f383badf03
Leo Molina <leonardomt@gmail.com>
2021-08-05 11:12:34 -0600

    rename

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from distutils.core import setup
 
 setup(
-    name = 'pupil_tracker',
-    packages = ['pupil_tracker'],
+    name = 'pupillometer',
     version = '0.0.1',
-    description = 'Pupil tracker',
+    packages = ['pupillometer'],
+    description = 'Pupil diameter tracker',
     author = 'Leo Molina',
     author_email = 'leonardomt@gmail.com',
     url = 'https://github.com/leomol/pupil-tracker',
     download_url = 'https://github.com/leomol/pupil-tracker/archive/refs/tags/v0.0.1.tar.gz',
-    keywords = ['pupil', 'tracker'],
-    classifiers = [],
+    keywords = ['pupillometer', 'pupillometry', 'pupil', 'tracker'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Image Processing',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3.8'
+    ],
 )
